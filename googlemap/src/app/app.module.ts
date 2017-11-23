67ed37958c40406034a2cc580f4078686b95ca6a
sungeun1019 <josungeun1019@gmail.com>
2017-11-23 11:16:44 +0900

    add

---
+++ googlemap/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCQcQlDx-8e2xhe4plQ-N1KX2rsFOeMO7w'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ googlemap/src/app/md/settingup_angular_googlemaps.md
@@ -0,0 +1,20 @@
+### Setting up Angular Google maps
+
+#### Install Angular Google Maps
+```
+npm intsall @agm/core --save
+```
+
+#### Marker display
+
+#### Map Functions & Event
+
+#### Map add, marker form
+
+
+References
+install 
+https://angular-maps.com/guides/getting-started/
+api key
+https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en#key
+https://stackoverflow.com/questions/46843606/how-to-make-custom-arrow-mark-in-agm-map
